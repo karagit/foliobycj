6ed1464249da6d472202eb601a4ecf6a3aeb8851
Carla Jean <carlajean@Carlas-Air.bbrouter>
2024-03-22 23:58:52 +0800

    Add coming soon page

---
+++ src/pages/_document.js
@@ -0,0 +1,17 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/x-icon" href="favicon.png" />
+      </Head>
+      <title>Portfolio | Carla Jean | Coming Soon</title>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ src/pages/coming-soon.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function() {
+  return (
+    <div className="coming-soon h-screen">
+      <div className="flex justify-center items-center flex-col h-full">
+        <h1 className="coming-soon__heading">COMING SOON</h1>
+        <p className="coming-soon__subheading">For any inquiries, you may contact me <a href="mailto:carlajeanmp@gmail.com" className="coming-soon__link">here</a></p>
+      </div>
+    </div>
+  )
+}
