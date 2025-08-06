e6915bdaf18cd253445ba5194a527fde0a365ec8
Carla Jean <carlajeanmp@gmail.com>
2025-08-06 12:47:51 +0800

    New layout initial

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
+import localFont from 'next/font/local';
 import "../styles/globals.scss";
 import { Analytics } from "@vercel/analytics/react";
+
+const veryVogueText = localFont({
+  src: '../fonts/VeryVogueText.otf',
+});
 
 export default function App({ Component, pageProps }) {
   return (
