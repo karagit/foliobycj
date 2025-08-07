cec95bdd3f083a95dafc4b6b09ea21d8181d1fbf
Carla Jean <carlajeanmp@gmail.com>
2025-08-08 00:45:05 +0800

    Fix works images in ios

--- src/Components/Tile.jsx
+++ src/Components/Tile.jsx
@@ -9,7 +9,7 @@ export default function Tile() {
           <div className="min-h-40 overflow-hidden rounded mb-5">
             <Link href={item.href} target="_blank" rel="noreferrer nofollow" className="tile-link relative">
               <span className="rounded py-1 px-2 text-xs bg-blue absolute z-10 top-2 right-2">{item.tag}</span>
-              <img src={item.src} className="object-cover object-left h-full" />
+              <img src={item.src} className="object-cover object-left h-full w-full" />
             </Link>
           </div>
           <p className="text-lg text-center">{item.name}</p>
